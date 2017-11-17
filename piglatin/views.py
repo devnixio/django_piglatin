6a410177fa874c52424abe8484ed39a4fceb522a
Chris Nixon <chris@devnix.io>
2017-11-17 14:28:52 -0600

    functional translation

--- piglatin/views.py
+++ piglatin/views.py
@@ -1,5 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def hello(request):
+def home(request):
     return render(request, 'home.html')
+
+def translate(request):
+    original = request.GET['originaltext'].lower()
+
+    translation = ''
+    for word in original.split():
+        if word[0] in ['a','e','i','o','u']:
+            # vowel
+            translation += word
+            translation += 'yay '
+        else:
+            # consonent
+            # heese
+            translation += word[1:]
+            # c
+            translation += word[0]
+            # ay
+            translation += 'ay '
+
+            # translation += word
+            # translation += 'consonent '
+
+    return HttpResponse(translation)
